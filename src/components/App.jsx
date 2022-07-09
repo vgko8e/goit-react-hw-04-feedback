4de07a79bb813563a2b4dadc132ec4365a22d724
vgko8e <92440969+vgko8e@users.noreply.github.com>
2022-07-09 17:24:33 +0300

    change name pjson

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,18 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import { Component } from 'react';
+
+export class App extends Component {
+  state = {
+    good: 0,
+    neutral: 0,
+    bad: 0,
+  };
+
+  render() {
+    return (
+      <div className="container">
+        {/* <Section title="Please leave feedback"></Section>
+        <Section title="Statistics"></Section> */}
+      </div>
+    );
+  }
+}
